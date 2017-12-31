d7738090199210285b9a94b81d0c3770bfccf208
Josh Dunn <joshdunn.uwo@gmail.com>
2017-12-31 17:46:27 -0500

    Better protect_with_forgery statement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+  protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 end
