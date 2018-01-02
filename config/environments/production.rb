83f9123ba4dee9d2626365d24972dc299c8fc9db
Josh Dunn <joshdunn.uwo@gmail.com>
2018-01-02 06:18:13 -0500

    update to https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.url = "http://pokeql.com"
+  config.url = "https://pokeql.com"
 end
