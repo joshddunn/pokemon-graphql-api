9742b5467b393541b34d6825ab7eb92e66ef2f06
Josh Dunn <joshdunn.uwo@gmail.com>
2018-01-03 19:57:01 -0500

    converted height and weight into more standard unit

--- app/graphql/types/pokemon_type.rb
+++ app/graphql/types/pokemon_type.rb
@@ -5,8 +5,16 @@ Types::PokemonType = GraphQL::ObjectType.define do
   field :id, !types.ID, "", property: :id
   field :identifier, !types.String, "", property: :identifier
   field :species, Types::PokemonSpecyType, "", property: :pokemon_specy
-  field :height, !types.Int, "", property: :height
-  field :weight, !types.Int, "", property: :weight
+  field :height, !types.Float, "" do 
+    resolve -> (obj, args, ctx) {
+      obj.height / 10.0 
+    }
+  end
+  field :weight, !types.Float, "" do
+    resolve -> (obj, args, ctx) {
+      obj.weight / 10.0
+    }
+  end
   field :baseExperience, !types.Int, "", property: :base_experience
   field :order, !types.Int, "", property: :order
   field :isDefault, !types.Boolean, "", property: :is_default
