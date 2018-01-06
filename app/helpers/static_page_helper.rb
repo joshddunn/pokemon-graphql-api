9313d3a57aac6382b0ef75e5627d2de4e1d95f4d
Josh Dunn <joshdunn.uwo@gmail.com>
2018-01-06 14:55:32 -0500

    use with indifferent access for json.parse

--- app/helpers/static_page_helper.rb
+++ app/helpers/static_page_helper.rb
@@ -4,6 +4,6 @@ module StaticPageHelper
     uri = URI("http://pokeql.com/v1?query={PokemonSpecies(filter:{id:#{number}}){edges{node{pokemons{identifier,sprites{silhouette{male{front}}normal{male{front}}}}}}}}")
     data = Net::HTTP.get_response(uri)
 
-    JSON.parse(data.body)["data"]["PokemonSpecies"]["edges"].first["node"]["pokemons"].first
+    JSON.parse(data.body).with_indifferent_access[:data][:PokemonSpecies][:edges].first[:node][:pokemons].first
   end
 end
