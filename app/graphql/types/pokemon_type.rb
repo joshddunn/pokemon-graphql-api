048821f10e2ebeaba9ce7733aa57400612df5970
Josh Dunn <joshdunn.uwo@gmail.com>
2018-03-21 19:41:04 -0400

    fix security hole and englishName

--- app/graphql/types/pokemon_type.rb
+++ app/graphql/types/pokemon_type.rb
@@ -40,7 +40,8 @@ Types::PokemonType = GraphQL::ObjectType.define do
     description "Returns the formatted english name of this pokemon"
     resolve -> (obj, args, ctx) {
       form_name = obj.pokemon_forms.map { |e| e.pokemon_form_names.joins(:language).find_by("languages.identifier": "en") }
-      return form_name.first.pokemon_name if form_name.present? and form_name.length == 1 and form_name.first.pokemon_name.present?
+
+      return form_name.first.pokemon_name if form_name.first.present? and form_name.length == 1 and form_name.first.pokemon_name.present?
 
       obj.pokemon_specy.pokemon_species_names.joins(:language).find_by("languages.identifier": "en").name
     }
